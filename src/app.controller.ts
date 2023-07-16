172ce296cf941effbaa21ca270c4ee3b2287eb4c
Alexandru Stefan <alex@ndru.dev>
2023-07-16 15:41:03 +0300

    feat: add support for all the pools from the sdk

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -18,4 +18,52 @@ export class AppController {
     const prices = await fetchAndParsePricesCsv(DATA_SOURCE.JITO_CSV);
     return calcAverageApy(prices);
   }
+
+  @Get('lido')
+  async getLidoApy(): Promise<any> {
+    const prices = await fetchAndParsePricesCsv(DATA_SOURCE.LIDO_CSV);
+    return calcAverageApy(prices);
+  }
+
+  @Get('laine')
+  async getLainApy(): Promise<any> {
+    const prices = await fetchAndParsePricesCsv(DATA_SOURCE.LAINE_CSV);
+    return calcAverageApy(prices);
+  }
+
+  @Get('cogent')
+  async getCogentApy(): Promise<any> {
+    const prices = await fetchAndParsePricesCsv(DATA_SOURCE.COGENT_CSV);
+    return calcAverageApy(prices);
+  }
+
+  @Get('solblaze')
+  async getSolBlazeApy(): Promise<any> {
+    const prices = await fetchAndParsePricesCsv(DATA_SOURCE.SOLBLAZE_CSV);
+    return calcAverageApy(prices);
+  }
+
+  @Get('socean')
+  async getSoceanApy(): Promise<any> {
+    const prices = await fetchAndParsePricesCsv(DATA_SOURCE.SOCEAN_CSV);
+    return calcAverageApy(prices);
+  }
+
+  @Get('daopool')
+  async getDaoPoolApy(): Promise<any> {
+    const prices = await fetchAndParsePricesCsv(DATA_SOURCE.DAOPOOL_CSV);
+    return calcAverageApy(prices);
+  }
+
+  @Get('jpool')
+  async getJPoolApy(): Promise<any> {
+    const prices = await fetchAndParsePricesCsv(DATA_SOURCE.JPOOL_CSV);
+    return calcAverageApy(prices);
+  }
+
+  @Get('everstake')
+  async getEverstakeApy(): Promise<any> {
+    const prices = await fetchAndParsePricesCsv(DATA_SOURCE.EVERSTAKE_CSV);
+    return calcAverageApy(prices);
+  }
 }
